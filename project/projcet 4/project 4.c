9c2183691d730e8231e3aa942e9bdef83950e529
fatima-el-ghalia <fatmalaaroussi12@gmail.com>
2025-09-23 09:25:56 +0100

    add

---
+++ project/projcet 4/project 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num ;
+    printf("write a number to check whether it is even or odd\n");
+    scanf("%d", &num);
+    if(num % 2 == 0){
+        printf ("the number %d is even" ,num);
+    }else{
+        printf("the number %d is odd" ,num);
+    }
+    return 0;
+}
---
+++ project/project 2/project 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    char name[50];
+    printf("write your name here\n");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+    return 0; 
+}
+
